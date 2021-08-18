be5fda14df3f89cca28b5974e7ae5556a98eae3a
lgorman <lgorman@turing.ac.uk>
2021-08-18 18:31:16 +0100

    adding a proper login component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, screen } from '@testing-library/react'
 
 import App from './App';
 
 it("App renders without crashing", () => {
-  const div = document.createElement("div")
-  ReactDOM.render(<App />, div)
+  render(<App />)
 });
--- src/components/data-query-component/__test__/data-query-component.test.js
+++ src/components/data-query-component/__test__/data-query-component.test.js
@@ -1,12 +1,49 @@
 import React from 'react'
-import ReactDOM from 'react-dom';
-import DataQueryComponent from '../data-query-component';
+import { DataQueryComponent } from '../data-query-component';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react'
 
-import { isTSAnyKeyword } from '@babel/types';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import '@testing-library/jest-dom';
 
-it("Data query component renders without crashing", () => {
-    // Create a div
-    const div = document.createElement("div")
-    // Attach our component to that div
-    ReactDOM.render(<DataQueryComponent />, div)
+// Setting up a mock server for doing
+// mock api requests
+const server = setupServer(
+    // Specifying an endpoint for the metadata requests
+    rest.get(process.env.REACT_APP_API_URL + 'api/metadata', (req, res, context) => {
+        return res(context.json({
+            greeting: 'hello there'
+        }))
+    })
+)
+
+
+// Specifying what needs to be done before,
+// during, and after each requests
+// The mock server has to be s
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+
+test("Data query component renders without crashing", () => {
+
+    // Loading the functions
+    const { getByText } = render(<DataQueryComponent />)
+
+    // Checking for the title
+    expect(getByText("RHoMIS 2.0 Data Querying")).not.toBeNull();
+    // Also simply could write this:
+    // getByText("RHoMIS 2.0 Data Querying")
+    // but I prefer to be a bit more explicit
+
+    // Checking for the card title
+    expect(getByText("Data Filters")).not.toBeNull();
+
+
+})
+
+
+test("Can fetch data and update filters accordingly", () => {
+    expect(true).toBe(true)
 })
