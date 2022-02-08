daae8ac1fe7385efee200dd1be6472df22164a1c
l-gorman <lgorman@turing.ac.uk>
2022-02-08 12:41:10 +0000

    adding scripts to publish

--- publish.sh
+++ publish.sh
@@ -5,22 +5,20 @@ CURRENTBRANCH=$(git branch --show-current)
 if [ "$CURRENTBRANCH" == "main" ]
 then 
     echo "Working in main branch" 
-    OLDDOMAIN="TESTDOMAIN"
-    NEWDOMAIN="api.l-gorman.com"
-    sed -i -e 's/$OLDDOMAIN/$NEWDOMAIN/g' ./public/CNAME
+    file_contents=$(<./public/CNAME)
+    echo "${file_contents//'TESTDOMAIN'/'app.l-gorman.com'}" > ./public/CNAME
     npm run build 
     npx gh-pages -d build -r https://github.com/l-gorman/rhomis-data-app
-    sed -i -e 's/$NEWDOMAIN/$OLDDOMAIN/g' ./public/CNAME
+    echo "$file_contents" > ./public/CNAME
 fi
 
 if [ "$CURRENTBRANCH" == "prod" ]
 then 
     echo "Working in main branch"
     npm run build
-    OLDDOMAIN="TESTDOMAIN"
-    NEWDOMAIN="api.rhomis.cgiar.org"
-    sed -i -e 's/$OLDDOMAIN/$NEWDOMAIN/g' ./public/CNAME
+    file_contents=$(<./public/CNAME)
+    echo "${file_contents//'TESTDOMAIN'/'rhomis.cgiar.org'}" > ./public/CNAME
     npx gh-pages -d build --repo https://github.com/ilri/rhomis-data-app
-    sed -i -e 's/$NEWDOMAIN/$OLDDOMAIN/g' ./public/CNAME
+    echo "$file_contents" > ./public/CNAME
 fi
 
