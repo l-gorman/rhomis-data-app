9605e3e64c0376cc2baefd3c13b4fe9861cfe353
l-gorman <lgorman@turing.ac.uk>
2022-02-08 10:06:32 +0000

    updating readme

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# RHoMIS 2.0 Front End Application
+
+
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app). 
--- src/components/authentication-component/AuthContext.js
+++ src/components/authentication-component/AuthContext.js
@@ -1,10 +1,14 @@
+
+
+// A component for creating the authentiction
+// context used throughout the application.
+
+// Context provider created in the main App.js
+// file in the src directory.
+
 import React, { createContext } from 'react';
 
-// const AuthContext = createContext(
-//     {
-//         authToken: null,
-//         setAuthToken: () => { }
-//     });
+
 const AuthContext = createContext()
 
 export default AuthContext;
