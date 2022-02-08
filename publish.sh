754fc374f57e3d1d6f1ef2cbf7005d6ed0e5ff44
l-gorman <lgorman@turing.ac.uk>
2022-02-08 12:50:32 +0000

    adding publish scripts

--- publish.sh
+++ publish.sh
@@ -4,21 +4,31 @@ CURRENTBRANCH=$(git branch --show-current)
 
 if [ "$CURRENTBRANCH" == "main" ]
 then 
-    echo "Working in main branch" 
+    echo "Working in staging branch (main)" 
+    # Read in the contents of CNAME
     file_contents=$(<./public/CNAME)
+    # Substitute "TESTDOMAIN" with the staging url
     echo "${file_contents//'TESTDOMAIN'/'app.l-gorman.com'}" > ./public/CNAME
+    # Build the app
     npm run build 
+    # Deploy using github pages app
     npx gh-pages -d build -r https://github.com/l-gorman/rhomis-data-app
+    # Restore the CNAME file
     echo "$file_contents" > ./public/CNAME
 fi
 
 if [ "$CURRENTBRANCH" == "prod" ]
 then 
-    echo "Working in main branch"
-    npm run build
+    echo "Working in staging branch"
+    # Reading in the contents of CNAME
     file_contents=$(<./public/CNAME)
+    # Replacing domains
     echo "${file_contents//'TESTDOMAIN'/'rhomis.cgiar.org'}" > ./public/CNAME
+    # Building the app
+    npm run build
+    # Deploy to ILRI repo
     npx gh-pages -d build --repo https://github.com/ilri/rhomis-data-app
+    # Restore th contents to original
     echo "$file_contents" > ./public/CNAME
 fi
 
