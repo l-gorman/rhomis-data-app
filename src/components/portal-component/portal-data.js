9916cdcd3d29b656209cc91c5b264072259161ad
Dave Mills <d.e.mills@stats4sd.org>
2022-05-07 15:09:48 +0100

    update survey-builder to use .env file; add setup instructions

--- src/components/portal-component/portal-data.js
+++ src/components/portal-component/portal-data.js
@@ -1,17 +1,17 @@
 // Copyright (C) 2022 Léo Gorman
-// 
+//
 // This file is part of rhomis-data-app.
-// 
+//
 // rhomis-data-app is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // rhomis-data-app is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with rhomis-data-app.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -26,7 +26,7 @@ export const PortalDataAll = [
         label: "surveyBuilder",
         text: "Get started straight away. Build a survey, collect test-data, and view your data.",
         icon: RiSurveyFill,
-        link: "https://rhomis-survey.stats4sdtest.online/login/",
+        link: process.env.REACT_APP_SURVEY_BUILDER_URL,
         external: true,
     },
     {
